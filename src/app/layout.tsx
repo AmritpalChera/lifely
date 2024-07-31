7ca955b9a3028d7ba1a9b52c91c242be4c956924
Amritpal Chera <52187061+AmritpalChera@users.noreply.github.com>
2024-07-31 14:03:21 -0400

    copy: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Studio',
-    default: 'Studio - Award winning developer studio based in Denmark',
+    template: '%s - Lifely',
+    default: 'Lifely - Personal and Career Growth Hacks',
   },
 }
 
